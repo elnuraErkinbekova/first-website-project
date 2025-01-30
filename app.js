b78d769cbccf4b3ee33137b2a718b96330e08eb4
Elnura <elnuraerkinbekova8@gmail.com>
2025-01-31 00:24:53 +0600

    create pricing cards

--- app.js
+++ app.js
@@ -11,3 +11,21 @@ menu.addEventListener("click", function() {
 // Function to make logos move
 const logos =document.querySelector(".rotating-logos__track").cloneNode(true);
 document.querySelector(".rotating-logos").appendChild(logos);
+
+//Function to toggle the monthly yearly plan
+function switchPricing() {
+    const checkbox = document.getElementById("pricing-toggle-checkbox");
+    const monthlyPrices = document.querySelectorAll(".monthly-price");
+    const yearlyPrices = document.querySelectorAll(".yearly-price");
+    const yearlyDiscount = document.querySelector(".save-percentage");
+
+    if(checkbox.checked) {
+        monthlyPrices.forEach((price) => price.classList.add("hidden"));
+        yearlyPrices.forEach((price) => price.classList.remove("hidden"));
+        yearlyDiscount.style.display = "inline";
+    }else{
+        monthlyPrices.forEach((price) => price.classList.remove("hidden"));
+        yearlyPrices.forEach((price) => price.classList.add("hidden"));
+        yearlyDiscount.style.display = "none";
+    }
+}
